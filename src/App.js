c05b32005f1259ddb0a1edbcde8db96671789d5c
Naoto Shioya <shioya.naoto17@gmail.com>
2022-09-05 23:40:36 +0900

    create environment

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import logo from "./logo.svg";
-import "./App.css";
 import { Amplify } from "aws-amplify";
 import { withAuthenticator } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
@@ -8,22 +6,7 @@ import awsExports from "./aws-exports";
 Amplify.configure(awsExports);
 
 function App({ signOut, user }) {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Task Manager Amplify</h2>
-        {user ? (
-          <>
-            <h3>権限持ちユーザー:{user.username}</h3>
-            <button onClick={signOut}>サインアウト</button>
-          </>
-        ) : (
-          <h3>権限がありません</h3>
-        )}
-      </header>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default withAuthenticator(App);
