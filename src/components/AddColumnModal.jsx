96ed3657d86e73d10160bd72b278325864706021
Naoto Shioya <shioya.naoto17@gmail.com>
2022-09-09 00:10:27 +0900

    create front design

--- src/components/AddColumnModal.jsx
+++ src/components/AddColumnModal.jsx
@@ -10,8 +10,15 @@ const AddColumnModal = (props) => {
         <div id="overlay" style={overlay}>
           <div id="modalContent" style={modalContent}>
             <h3 style={inputTitle}>Column name</h3>
-            <input></input>
-            <button onClick={closeModal}>Close</button>
+            <div style={inputWrapper}>
+              <input style={input} placeholder="add column name!" />
+              <button style={submitButton} className="button">
+                Submit
+              </button>
+            </div>
+            <button style={closeButton} onClick={closeModal} className="button">
+              ×
+            </button>
           </div>
         </div>
       ) : (
@@ -22,9 +29,11 @@ const AddColumnModal = (props) => {
 };
 
 const modalContent = {
-  background: "white",
-  padding: "10px",
-  borderRadius: "3px",
+  background: "#E2E8F0",
+  padding: "40px",
+  width: "480px",
+  height: "220px",
+  borderRadius: "8px",
 };
 
 const overlay = {
@@ -42,6 +51,46 @@ const overlay = {
 
 const inputTitle = {
   color: "#30363d",
+};
+
+const inputWrapper = {
+  display: "flex",
+  marginTop: "12px",
+};
+
+const input = {
+  width: "100%",
+  height: "40px",
+  paddingLeft: "12px",
+  borderRadius: "8px",
+  border: ".8px solid #718096",
+  outline: "none",
+  background: "#EDF2F7",
+};
+
+const submitButton = {
+  marginLeft: "12px",
+  cursor: "pointer",
+  padding: "0 12px",
+  borderRadius: "8px",
+  border: ".4px solid #4A5568",
+  background: "#718096",
+  color: "#EDF2F7",
+  transition: "opacity 0.2s",
+};
+
+const closeButton = {
+  margin: "0 auto",
+  display: "block",
+  marginTop: "40px",
+  cursor: "pointer",
+  height: "40px",
+  padding: "0 16px",
+  borderRadius: "8px",
+  border: ".4px solid #CBD5E0",
+  background: "#fff",
+  color: "#4A5568",
+  transition: "opacity 0.2s",
 };
 
 export default AddColumnModal;
