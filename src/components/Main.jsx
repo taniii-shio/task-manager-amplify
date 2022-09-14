694e640d08aed82483922b5724868dd248b81f7f
Naoto Shioya <shioya.naoto17@gmail.com>
2022-09-14 13:46:41 +0900

    add delete task func

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,10 +3,11 @@ import { API } from "aws-amplify";
 import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 import { AiOutlineDelete } from "react-icons/ai";
 
-import { listColumns } from "../graphql/queries";
+import { getColumn, listColumns } from "../graphql/queries";
 import Card from "./Card";
 import AddColumnModal from "./AddColumnModal";
 import AddTaskModal from "./AddTaskModal";
+import { deleteColumn } from "../graphql/mutations";
 
 const Main = () => {
   const [data, setData] = useState([]);
@@ -31,7 +32,33 @@ const Main = () => {
     }
   };
 
-  console.log(data);
+  const deleteColumnHandler = async (e) => {
+    try {
+      if (!e.target.id) return;
+      console.log(e.target.id);
+      const targetColumn = await API.graphql({
+        query: getColumn,
+        variables: {
+          id: e.target.id,
+        },
+        authMode: "AMAZON_COGNITO_USER_POOLS",
+      });
+      console.log(targetColumn.data.getColumn._version);
+      await API.graphql({
+        query: deleteColumn,
+        variables: {
+          input: {
+            id: e.target.id,
+            _version: targetColumn.data.getColumn._version,
+          },
+        },
+        authMode: "AMAZON_COGNITO_USER_POOLS",
+      });
+      fetchData();
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   const handleShowAddColumnModal = () => {
     setShowAddColumnModal(true);
@@ -84,53 +111,61 @@ const Main = () => {
   return (
     <DragDropContext onDragEnd={onDragEnd}>
       <div className="trello">
-        {data.map((section) => (
-          <Droppable key={section.id} droppableId={section.id}>
-            {(provided) => (
-              <div
-                className="trello-section"
-                ref={provided.innerRef}
-                {...provided.droppableProps}
-              >
-                <div className="trello-section-title-wrapper">
-                  <div className="trello-section-title">{section.title}</div>
-                  <AiOutlineDelete className="trello-section-delete-button" />
-                </div>
-                <div className="trello-section-content">
-                  {section.tasks.map((task, index) => (
-                    <Draggable
-                      draggableId={task.id}
-                      index={index}
-                      key={task.id}
+        {data.map((section) =>
+          section._deleted ? (
+            <div key={section.id}></div>
+          ) : (
+            <Droppable key={section.id} droppableId={section.id}>
+              {(provided) => (
+                <div
+                  className="trello-section"
+                  ref={provided.innerRef}
+                  {...provided.droppableProps}
+                >
+                  <div className="trello-section-title-wrapper">
+                    <div className="trello-section-title">{section.title}</div>
+                    <AiOutlineDelete
+                      className="trello-section-delete-button"
+                      id={section.id}
+                      onClick={(e) => deleteColumnHandler(e)}
+                    />
+                  </div>
+                  <div className="trello-section-content">
+                    {section.tasks.map((task, index) => (
+                      <Draggable
+                        draggableId={task.id}
+                        index={index}
+                        key={task.id}
+                      >
+                        {(provided, snapshot) => (
+                          <div
+                            ref={provided.innerRef}
+                            {...provided.draggableProps}
+                            {...provided.dragHandleProps}
+                            style={{
+                              ...provided.draggableProps.style,
+                              opacity: snapshot.isDragging ? "0.5" : "1",
+                            }}
+                          >
+                            <Card>{task.title}</Card>
+                          </div>
+                        )}
+                      </Draggable>
+                    ))}
+                    {provided.placeholder}
+                    <div
+                      className="add-task-card"
+                      id={section.id}
+                      onClick={(e) => handleShowAddTaskModal(e)}
                     >
-                      {(provided, snapshot) => (
-                        <div
-                          ref={provided.innerRef}
-                          {...provided.draggableProps}
-                          {...provided.dragHandleProps}
-                          style={{
-                            ...provided.draggableProps.style,
-                            opacity: snapshot.isDragging ? "0.5" : "1",
-                          }}
-                        >
-                          <Card>{task.title}</Card>
-                        </div>
-                      )}
-                    </Draggable>
-                  ))}
-                  {provided.placeholder}
-                  <div
-                    className="add-task-card"
-                    id={section.id}
-                    onClick={(e) => handleShowAddTaskModal(e)}
-                  >
-                    <p>+ Add task</p>
+                      <p id={section.id}>+ Add task</p>
+                    </div>
                   </div>
                 </div>
-              </div>
-            )}
-          </Droppable>
-        ))}
+              )}
+            </Droppable>
+          )
+        )}
         <div className="add-column-section" onClick={handleShowAddColumnModal}>
           <p>+ Add column</p>
         </div>
